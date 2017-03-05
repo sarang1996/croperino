5f8bef58e601f3e225e962cfcd483eb7ca77a212
sarang1996 <sarangkachhadiya@gmail.com>
2017-03-05 11:16:17 +0530

    intial one

---
+++ app/src/main/java/com/yashmewada/dialog/Croper/CroperinoFileUtil.java
@@ -0,0 +1,153 @@
+package com.yashmewada.dialog.Croper;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class CroperinoFileUtil {
+
+    public static final int REQUEST_EXTERNAL_STORAGE = 1;
+    public static final int REQUEST_CAMERA = 2;
+    public static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static File mFileTemp;
+
+    public static File getmFileTemp() {
+        return mFileTemp;
+    }
+
+    public static void setupDirectory(Context ctx) {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            if (Environment.getExternalStorageDirectory().exists()) {
+                mFileTemp = new File(Environment.getExternalStorageDirectory() + CroperinoConfig.getsDirectory(), CroperinoConfig.getsImageName());
+            } else {
+                mFileTemp = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + CroperinoConfig.getsDirectory(), CroperinoConfig.getsImageName());
+            }
+        } else {
+            mFileTemp = new File(ctx.getFilesDir(), CroperinoConfig.getsImageName());
+        }
+
+        if (!mFileTemp.exists()) {
+            mFileTemp = new File(CroperinoConfig.getsRawDirectory());
+            mFileTemp.mkdirs();
+        }
+    }
+
+    public static File newCameraFile() throws IOException {
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Camera");
+        if(!storageDir.exists()) {
+            storageDir.mkdir();
+        }
+        mFileTemp = File.createTempFile(CroperinoConfig.getsImageName(), ".jpg", storageDir);
+        return mFileTemp;
+    }
+
+    public static File newGalleryFile(Intent data, Context ctx) {
+        try {
+            mFileTemp = new File(CroperinoConfig.getsRawDirectory() + CroperinoConfig.getsImageName());
+            copyFile(new File(CroperinoFileUtil.getPath(ctx, data.getData())), mFileTemp);
+            return mFileTemp;
+        } catch (Exception e) {
+            if(e instanceof  IOException) {
+                mFileTemp = new File(CroperinoFileUtil.getPath(ctx, data.getData()));
+            } else {
+                Toast.makeText(ctx, "Gallery is empty or access is prohibited by device.", Toast.LENGTH_SHORT).show();
+            }
+            return mFileTemp;
+        }
+
+    }
+
+    private static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!sourceFile.exists()) {
+            return;
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+        source = new FileInputStream(sourceFile).getChannel();
+        destination = new FileOutputStream(destFile).getChannel();
+        if (destination != null && source != null) {
+            destination.transferFrom(source, 0, source.size());
+        }
+        if (source != null) {
+            source.close();
+        }
+        if (destination != null) {
+            destination.close();
+        }
+    }
+
+    public static Boolean verifyStoragePermissions(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int writePermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            int readPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (writePermission != PackageManager.PERMISSION_GRANTED || readPermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        activity,
+                        PERMISSIONS_STORAGE,
+                        REQUEST_EXTERNAL_STORAGE
+                );
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public static Boolean verifyCameraPermissions(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            int cameraPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.CAMERA);
+
+            if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        activity,
+                        new String[]{(Manifest.permission.CAMERA)},
+                        REQUEST_CAMERA
+                );
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public static String getPath(final Context context, final Uri uri) {
+        String result;
+        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+        if (cursor == null) {
+            result = uri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/yashmewada/dialog/MainActivity.java
@@ -0,0 +1,66 @@
+package com.yashmewada.dialog;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Observable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.yashmewada.dialog.Croper.Croperino;
+import com.yashmewada.dialog.Croper.CroperinoConfig;
+import com.yashmewada.dialog.Croper.CroperinoFileUtil;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.demo_image);
+
+        new CroperinoConfig("IMG_" + System.currentTimeMillis() + ".jpg", "/punchit", "/sdcard/punchit");
+        CroperinoFileUtil.verifyStoragePermissions(this);
+        CroperinoFileUtil.setupDirectory(MainActivity.this);
+    }
+
+    public void demo(View view) {
+        Croperino.prepareGallery(MainActivity.this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case CroperinoConfig.REQUEST_TAKE_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    /* Parameters of runCropImage = File, Activity Context, Image is Scalable or Not, Aspect Ratio X, Aspect Ratio Y, Button Bar Color, Background Color */
+                    Croperino.runCropImage(CroperinoFileUtil.getmFileTemp(), MainActivity.this, true, 1, 2, 0, 0);
+                }
+                break;
+            case CroperinoConfig.REQUEST_PICK_FILE:
+                if (resultCode == Activity.RESULT_OK) {
+                    CroperinoFileUtil.newGalleryFile(data, MainActivity.this);
+                    Croperino.runCropImage(CroperinoFileUtil.getmFileTemp(), MainActivity.this, false, 1, 2, 0, 0);
+                }
+                break;
+            case CroperinoConfig.REQUEST_CROP_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri i = Uri.fromFile(CroperinoFileUtil.getmFileTemp());
+                    imageView.setImageURI(i);
+                    //Do saving / uploading of photo method here.
+                    //The image file can always be retrieved via CroperinoFileUtil.getmFileTemp()
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
